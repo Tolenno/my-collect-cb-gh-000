191372d00d2321aa1ae5cf6f40346b6ae1ce0833
Matt Hough <matt@hough.tv>
2017-09-26 08:00:11 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,18 @@
 array = ["Time", "Tom", "Bob"]
 
 def my_collect(array)
-  
+  index = 0
+  collection = []
+  while index < array.size
+    collection << yield(array[index])
+    index += 1
+  end
+  collection
 end
 
 1. count the current index we are on
 2. create a new collection array to store results
-3. while the index we are on is less than the number of indices in the code, run the below code
+3. while the index we are on is less than the array.size, run the below code
 4. yield the array[index] to the new collection array
 5. Increase the index number by one
 6. Exit the while loop
