9d8f427a3092c52337c1061a1d4474a06a4e67c7
Matt Hough <matt@hough.tv>
2017-09-26 08:05:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,15 +8,15 @@ def my_collect(array)
       collection << yield(array[index]) #yield the array[index] to the new collection array
       index += 1 #increase the index number by one - can now use next array index
     end
-    collection
+    collection #return the collection array
   else
-    "Wat."
+    "Wat." #did not give a block
 end
 
 
 
 3.
-4. 
-5. 
+4.
+5.
 6. Exit the while loop
 7. Return the collection array
