754abade42bff8083224650cce3859cc3e12c711
Matt Hough <matt@hough.tv>
2017-09-26 07:58:06 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,3 +3,11 @@ array = ["Time", "Tom", "Bob"]
 def my_collect(array)
   
 end
+
+1. count the current index we are on
+2. create a new collection array to store results
+3. while the index we are on is less than the number of indices in the code, run the below code
+4. yield the array[index] to the new collection array
+5. Increase the index number by one
+6. Exit the while loop
+7. Return the collection array
